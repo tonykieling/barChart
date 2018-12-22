5992740cda212719447f56d5c5291b2858e9d14c
Tony Kieling <tony.kieling@gmail.com>
2018-12-22 21:42:03 +0000

    chartLabel ok but building a function to calculate for any text in the chart

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -52,7 +52,7 @@ $(function(){
     // +++++++++++++++++++++++++++++++++++++++
     // ++++++++++ BIG FRAME ELEMENT ++++++++++
     // +++++++++++++++++++++++++++++++++++++++
-    //big frame which will host all elements, including the bar chart
+    // big frame which will host all elements, including the bar chart
     // size as received via agrguments
     let bigFrame = $("<div></div>");
     $(element).append(bigFrame);
@@ -60,28 +60,70 @@ $(function(){
       position: "relative",
       left: 50, // check this out later
       width: option.frameWidth,
-      height: option.frameHeigth,
+      height: option.frameHeight,
       backgroundColor: "white",
     });
 
-
+    var frameHeight = option.frameHeight * 0.7;    //old way
+    var frameWidth = option.frameWidth * 0.7;
 
 
     // +++++++++++++++++++++++++++++++++++++++++
     // ++++++++++ CHART LABEL ELEMENT ++++++++++
     // +++++++++++++++++++++++++++++++++++++++++
     //set the chart label, regarding the argument received from options
+
     let chartLabel = $("<div></div>");
     $(bigFrame).append(chartLabel);
     $(chartLabel).html(option.chartLabelText);
     $(chartLabel).css({
-      fontSize: 30,   // later, set a maximum limit such as 45 and minimum as 12. Default should be 30.
-      position: "relative",
+      position: "absolute",
       textAlign: "center",
+      width: "100%",
       top: 0,
+      fontFamily: `${option.chartLabelFontFamily}, "Arial"`,
+      fontSize: "8px",   // later, set a maximum limit such as 45 and minimum as 12. Default should be 30.
       color: option.chartLabelColor,
+      border: option.chartLabelBorder,
     });
 
+
+
+
+
+
+
+
+
+
+
+
+    const tempFontSize = (parseInt($(chartLabel).css("fontSize")));
+    const heightMax = option.frameHeight * 0.1;
+
+
+    let countFontSize = 1;
+    // build a function to calculate the size of font or width or height for any call (5 text elements), in order to be better fittable
+    // if (par = "w"){ //deal with height
+
+    // } else (par = "h"){ //deal with width
+
+    // }
+
+    while ((parseInt($(chartLabel).css("height"))) < heightMax){
+      $(chartLabel).css("fontSize", (tempFontSize + countFontSize));
+      countFontSize++;
+
+      if (countFontSize > 50){    // just in case to avoid infinite loops
+        break;
+      }
+    }
+
+    if ((parseInt(option.chartLabelFontSize)) < (parseInt($(chartLabel).css("fontSize")))){
+      if ((parseInt(option.chartLabelFontSize)) > 8){ //minumum size
+        $(chartLabel).css("fontSize", parseInt(option.chartLabelFontSize));
+      }
+    }
 
 
 
@@ -89,33 +131,30 @@ $(function(){
     // ++++++++++ FRAME ELEMENT ++++++++++++++
     // +++++++++++++++++++++++++++++++++++++++
     let biggestNumber = Math.max(...argum);;
-    let ceilChart = biggestNumber * 1.02;   //it gives a offset to the frame chart to the biggest column
+    let ceilChart = biggestNumber * 1.02;   //it gives a offset to the frame chart related to the biggest column
     if (option.setMaximunValue){
       ceilChart = option.setMaximunValue; //change the frame reference to the biggest value if the user say so
     }
 
-    if (biggestNumber % 5 != 0){
-      let count = 1;
-      while ((ceilChart % 5) != 0){
-        ceilChart = Math.floor(ceilChart) + count;
-        count++;
-      }
-    }
-
+    // it is used only in absolute mode, note percent mode
+    // maybe it is not mattering
+    // if (biggestNumber % 5 != 0){
+    //   let count = 1;
+    //   while ((ceilChart % 5) != 0){
+    //     ceilChart = Math.floor(ceilChart) + count;
+    //     count++;
+    //   }
+    // }
+// console.log(((parseInt($(chartLabel).css("height"))) * 0.4) + " chartLabelH: " + $(chartLabel).css("height"));
     //frame to the bar chart
     var frame = $("<div></div>");
     $(bigFrame).append(frame);
-
-    var frameHeigth = option.frameHeigth * 0.75;    //old way
-    var frameWidth = option.frameWidth * 0.7;
-
-
     $(frame).css({
       position: "absolute",
       left: 80,    //   TODO: first draw the frame and after position that
-      top: (parseInt($(chartLabel).css("height"))) + 30,  //distance related to the chartlabel
+      top: (parseInt($(chartLabel).css("height"))) + ((parseInt($(chartLabel).css("height"))) * 0.4),  //distance related to the chartlabel
       width: frameWidth,
-      height: frameHeigth,
+      height: frameHeight,
       backgroundColor: "aquamarine",   //set default and user's option
     });
 
@@ -128,9 +167,9 @@ $(function(){
     let lineY = $("<div></div>");
     $(frame).append(lineY);
     $(lineY).css({
-      height: frameHeigth * 1.05, // the arrow's line is gonna be 5% bigger than the frame heigth
+      height: frameHeight * 1.05, // the arrow's line is gonna be 5% bigger than the frame heigth
       position: "absolute",
-      bottom: -1 * (((frameHeigth * 1.05) - frameHeigth) / 2),
+      bottom: -1 * (((frameHeight * 1.05) - frameHeight) / 2),
       borderLeft: "solid black",
       zIndex: 15,
     });
@@ -138,7 +177,7 @@ $(function(){
     $(lineY).append(arrowY);
     $(arrowY).css({
       position: "absolute",
-      top: -1 * (((frameHeigth * 1.03) - frameHeigth) / 4),
+      top: -1 * (((frameHeight * 1.03) - frameHeight) / 4),
       left: -5,
       border: "solid black",
       borderWidth: "0 2px 2px 0",
@@ -175,6 +214,21 @@ $(function(){
     });
 
 
+    //**************************************************************have to adjust the arrow's details for small and big frames */
+    if (((option.frameHeight) <= 400) || ((option.frameWidth) <= 600)){
+      $(lineX).css("borderBottom", "solid black 1.5px");
+      $(arrowX).css("borderWidth", "0 1.5px 1.5px 0");
+      $(lineY).css("borderLeft", "solid black 1.5px");
+      $(arrowY).css("borderWidth", "0 1.5px 1.5px 0");
+    }
+
+    if (((option.frameHeight) <= 200) || ((option.frameWidth) <= 400)){
+      $(lineX).css("borderBottom", "solid black 1px");
+      $(arrowX).css("borderWidth", "0 1px 1px 0");
+      $(lineY).css("borderLeft", "solid black 1px");
+      $(arrowY).css("borderWidth", "0 1px 1px 0");
+    }
+
 
 
     // +++++++++++++++++++++++++++++++++++++++++++++
@@ -186,7 +240,7 @@ $(function(){
       numberOfDivisions = option.numberOfDivisionsYAxis; //change default if user says
     }
 
-    let divisionsHeigth = frameHeigth /numberOfDivisions;
+    let divisionsHeigth = frameHeight /numberOfDivisions;
     let topDivisionPos = divisionsHeigth;
 
 
@@ -290,7 +344,7 @@ $(function(){
         left: vleft,
         backgroundColor: "blue",
         width: columnWidth,
-        height: ((argum[i] / ceilChart) * frameHeigth),
+        height: ((argum[i] / ceilChart) * frameHeight),
         zIndex: 10,
       });
       $(frame).append(columns[i]);
@@ -327,6 +381,9 @@ $(function(){
           } else if (option.setLabelColumnPos.toLowerCase() == "middle"){
             $(labelColumns[i]).css("bottom",
                               (((parseInt($(columns[i]).css("height"))) - (parseInt($(labelColumns[i]).css("height")))) / 2));
+          } else if (option.setLabelColumnPos.toLowerCase() == "over"){
+            $(labelColumns[i]).css("bottom",
+                              ((parseInt($(columns[i]).css("height")))));
           } else {
             console.log("columnposition: gonna be top (default)");
             $(labelColumns[i]).css("top", 0);
@@ -341,13 +398,17 @@ $(function(){
       // console.log(typeof(option.setXLabel));
       if (i == 0) { // it will set the label , or its variable, only in the very first time
         if (typeof(option.setXLabel) == "string"){
-          if (((option.setXLabel.toLowerCase()) == "month") ||
-             ((option.setXLabel.toLowerCase()) == "number") ||
-             ((option.setXLabel.toLowerCase()) == "on")) {
+          if (((option.setXLabel.toLowerCase()) == "month") || ((option.setXLabel.toLowerCase()) == "on")) {
             eachXLabel = setXLabelMonth(argum.length, option.setXLabelStarts);
+          } else if ((option.setXLabel.toLowerCase()) == "number"){
+            console.log("now is number");
+            if ((typeof(Number(option.setXLabel))) == "number"){
+              console.log("OK, but need to have information from the user");
+            }
+          }
           }
         }
-      }
+      // }
 
       labelAxisColumn[i] = $("<div></div>");
       $(columns[i]).append(labelAxisColumn[i]);
@@ -359,6 +420,24 @@ $(function(){
         fontSize: "1.4vw",
         textAlign:"center",
       });
+
+      // setup the fontSize of the labelAxisColumn HERE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
@@ -382,22 +461,42 @@ $(function(){
   console.log("distance is: " + distancek);
 
   // $(frame).css("height", distancek);
-  // var frameHeigth = distancek;
+  // var frameHeight = distancek;
   // var frameWidth = option.frameWidth * 0.7;
   */
+
+
 
   }
 
 // barC([100, 201, 307, 600, 799, 878, 130, 700, 588, 80, 110, 50, 60, 70],
 // barC([100, 201, 307, 600, 799, 878, 130, 700, 588],
-  barC([100, 201, 307, 600, 799, 70],
-   {frameHeigth: 500, frameWidth: 700,
+  barC(
+    // FIRST ARGUMENT
+    //  data to be considered in the chart:
+    [100, 201, 307, 600, 799, 70],
+
+    // SECOND ARGUMENT
+    //  chart options
+    {
+    //bigFrame Features:
+    frameHeight: 500, frameWidth: 700, /* consider color and border features*/
+
+    //chartLabelFeatures:
+    chartLabelText: "ChartLabel Test", chartLabelFontFamily: "Serif", chartLabelFontSize: "60", chartLabelColor: "green",
+     chartLabelBorder: "solid red",
+
+    //chart features:
     setXLabel: "month", setXLabelStarts: "nov",
-    setMaximunValue: 0,
-     xLabelText: "Monthly $ spend", chartLabelText: "This is the bar chart name", chartLabelColor: "green",
-     setColumnsFont: 0, setColumnWithLabel: true, setLabelColumnPos: "middle", /*top, bottom, middle */
-     numberOfDivisionsYAxis: 5, typeOfDivision: "percent", setDivisionsOverColumns: 0},
-   "#barChartPlace");
+      setMaximunValue: 0,
+      xLabelText: "Monthly $ spend",
+      setColumnsFont: 0, setColumnWithLabel: true, setLabelColumnPos: "over", /*top, bottom, middle and over*/
+      numberOfDivisionsYAxis: 5, typeOfDivision: "absolute", setDivisionsOverColumns: 0},
+
+    // THIRD ARGUMENT
+    //  chart position, that is, in what element it's supposed to be placed
+    "#barChartPlace"
+   );
 
 
   //  barC({["jan", 100], ["feb", 200], ["mar", 150], ["apr", 230]});
